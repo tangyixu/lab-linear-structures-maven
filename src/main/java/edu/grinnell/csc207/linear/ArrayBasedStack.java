aaaa1e7d23e3491a09284a46a18c6e2c1bd2a4cb
tangyixu <tangyixu@grinnell.edu>
2024-10-09 22:25:40 -0500

    Squash bugs in ArrayBasedStack.java

--- src/main/java/edu/grinnell/csc207/linear/ArrayBasedStack.java
+++ src/main/java/edu/grinnell/csc207/linear/ArrayBasedStack.java
@@ -7,7 +7,7 @@ import java.util.NoSuchElementException;
  * A simple array-based stack.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Tiffany Tang
  */
 public class ArrayBasedStack<T> implements Stack<T> {
   // +--------+----------------------------------------------------------
@@ -64,6 +64,7 @@ public class ArrayBasedStack<T> implements Stack<T> {
     if (this.isFull()) {
       throw new Exception("full");
     } // if full
+    this.values[size] = val;
     this.size++;
   } // put(T)
 
